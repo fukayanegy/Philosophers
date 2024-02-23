dba5d69cfe0f3c83100682d3b5d5af70b4808219
Eito Takahama <etakaham@c1r12s9.42tokyo.jp>
2024-02-23 18:13:24 +0900

    edit isdigit

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,24 +6,38 @@
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 16:32:03 by etakaham          #+#    #+#             */
-/*   Updated: 2024/02/23 17:48:36 by etakaham         ###   ########.fr       */
+/*   Updated: 2024/02/23 18:12:18 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../headers/philosophers.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	int	number_of_philosophers;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
+	int	i;
 
-	number_of_philosophers = 5;
-	time_to_die = ft_atoi("8");
-	time_to_eat = ft_atoi("80");
-	time_to_sleep = ft_atoi("18");
-
-	printf("start. %d %d %d\n", time_to_die, time_to_eat, time_to_sleep);
+	if (argc != 5 && argc != 6)
+	{
+		printf("hoge\n");
+		return (1);
+	}
+	i = 1;
+	while (i < argc)
+	{
+		if (!ft_isdigit(argv[i]))
+		{
+			printf("neko\n");
+			return (1);
+		}
+		i++;
+	}
+	number_of_philosophers = ft_atoi(argv[1]);
+	time_to_die = ft_atoi(argv[2]);
+	time_to_eat = ft_atoi(argv[3]);
+	time_to_sleep = ft_atoi(argv[4]);
 	return (0);
 }
--- philo/srcs/utils/ft_isdigit.c
+++ philo/srcs/utils/ft_isdigit.c
@@ -1,20 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   function.h                                         :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: etakaham <etakaham@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/23 17:44:49 by etakaham          #+#    #+#             */
-/*   Updated: 2024/02/23 18:08:59 by etakaham         ###   ########.fr       */
+/*   Created: 2024/02/23 18:03:15 by etakaham          #+#    #+#             */
+/*   Updated: 2024/02/23 18:09:25 by etakaham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FUNCTION_H
-# define FUNCTION_H
+# include "../../headers/philosophers.h"
 
-/*utils*/
-int		ft_atoi(const char *str);
-bool	ft_isdigit(const char *str);
+static bool	_isdigit(char c)
+{
+	return ('0' <= c && c <= '9');
+}
 
-#endif
+bool	ft_isdigit(const char *str)
+{
+	while (*str != '\0')
+	{
+		if (!_isdigit(*str))
+			return false;
+		str++;
+	}
+	return true;
+}
